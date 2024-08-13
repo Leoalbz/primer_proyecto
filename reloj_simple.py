6f3d5c12c2af677437ce54aedb23d0fcaac9db53
Leoalbz <albornozleonardoagustin@gmail.com>
2024-08-13 10:52:42 -0300

    agregar el proyecto

---
+++ reloj_simple.py
@@ -0,0 +1,15 @@
+# Proyecto 2: Reloj simple
+import tkinter as tk
+import time
+ventana = tk.Tk()
+ventana.title('Reloj simple')
+ventana.geometry('400x200')
+reloj = tk.Label(ventana, font =
+('Arial', 60), bg = 'blue', fg = 'white')
+def hora():
+    tiempo_actual =time.strftime('%H:%M')
+    reloj.config(text = tiempo_actual)
+ventana.after(1000, hora)
+reloj.pack(anchor = 'center')
+hora()
+ventana.mainloop()
