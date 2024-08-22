f5f5786ec6ca7f22a5d933481f625124d5598792
Bruno Percara <bruno.percara@gmail.com>
2024-08-21 21:24:43 -0300

    add paciente_id
    se agregó id de paciente para el manejo de las listas

--- clientes.py
+++ clientes.py
@@ -1,5 +1,6 @@
 class paciente:
-    def __init__(self, nombre, raza, edad, diagnostico, tratamiento):
+    def __init__(self, id, nombre, raza, edad, diagnostico, tratamiento):
+        self.id = int(id)
         self.nombre = nombre
         self.raza = raza
         self.edad = edad
@@ -8,6 +9,7 @@ class paciente:
 
     def __str__(self):
         datos = { 
+            "Id": self.id,
             "Nombre": self.nombre,
             "Raza": self.raza,
             "Edad": self.edad,
@@ -26,18 +28,28 @@ class lista_pacientes:
   
         def agregar_paciente(self, paciente):
             self.paciente.append(paciente)
-            print(f"Se agregó al paciente '{paciente.nombre}' a la lista de pacientes")
+            print(f"Se agregó al paciente '{paciente.nombre}' a la lista de pacientes\n")
 
         def mostrar_pacientes(self):
             if self.paciente:
                 print("Lista de pacientes:")
             for paciente in self.paciente:
                 print(paciente)
-            
+        
+        def eliminar_paciente(self, paciente_id):
+            for paciente in self.paciente:
+                if paciente.id == paciente_id:
+                    self.paciente.remove(paciente)
+                    print(f"Se elimino al paciente {paciente.nombre} con Id {paciente.id} de la lista de pacientes\n")
 
 pacientes = lista_pacientes()
 
 
-pacientes.agregar_paciente(paciente("Tito","Bulldog","4","Intoxicacion por alimento inadecuado","comprimidos gastricos"))
-pacientes.agregar_paciente(paciente("Maria","Persian","2","Sistitis","Inyectables + Comprimidos"))
+pacientes.agregar_paciente(paciente(1,"Tito","Bulldog","4","Intoxicacion por alimento inadecuado","comprimidos gastricos"))
+pacientes.agregar_paciente(paciente(2,"Maria","Persian","2","Cistitis","Inyectables + Comprimidos"))
+
+pacientes.mostrar_pacientes()
+
+pacientes.eliminar_paciente(2)
+
 pacientes.mostrar_pacientes()
