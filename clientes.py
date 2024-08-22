5bb0fb34312d6ed048453162fac54d0cc137ce22
Bruno Percara <bruno.percara@gmail.com>
2024-08-22 19:51:26 -0300

    add Gestion en poo
    gestion en poo

--- clientes.py
+++ clientes.py
@@ -1,6 +1,6 @@
 class paciente:
     def __init__(self, id, nombre, raza, edad, diagnostico, tratamiento):
-        self.id = int(id)
+        self.id = id
         self.nombre = nombre
         self.raza = raza
         self.edad = edad
@@ -23,33 +23,26 @@ class paciente:
         return formato_salida
 
 class lista_pacientes:
-        def __init__(self):
-            self.paciente = []
+    def __init__(self):
+        self.paciente = []
   
-        def agregar_paciente(self, paciente):
-            self.paciente.append(paciente)
-            print(f"Se agregó al paciente '{paciente.nombre}' a la lista de pacientes\n")
+    def agregar_paciente(self, paciente):
+        self.paciente.append(paciente)
+        print(f"Se agregó al paciente '{paciente.nombre}' a la lista de pacientes\n")
 
-        def mostrar_pacientes(self):
-            if self.paciente:
-                print("Lista de pacientes:")
-            for paciente in self.paciente:
-                print(paciente)
+    def mostrar_pacientes(self):
+        if self.paciente:
+            datos = "\n".join(str(paciente) for paciente in self.paciente) 
+        else:
+            datos = "No hay pacientes en la lista"
+        return datos
         
-        def eliminar_paciente(self, paciente_id):
-            for paciente in self.paciente:
-                if paciente.id == paciente_id:
-                    self.paciente.remove(paciente)
-                    print(f"Se elimino al paciente {paciente.nombre} con Id {paciente.id} de la lista de pacientes\n")
+    def eliminar_paciente(self, paciente_id):
+        for paciente in self.paciente:
+            if paciente.id == paciente_id:
+                self.paciente.remove(paciente)
+                print(f"Se elimino al paciente {paciente.nombre} con Id {paciente.id} de la lista de pacientes\n")
+                return
+            
+        print(f"No se encontró ningún paciente con Id {paciente_id}")
 
-pacientes = lista_pacientes()
-
-
-pacientes.agregar_paciente(paciente(1,"Tito","Bulldog","4","Intoxicacion por alimento inadecuado","comprimidos gastricos"))
-pacientes.agregar_paciente(paciente(2,"Maria","Persian","2","Cistitis","Inyectables + Comprimidos"))
-
-pacientes.mostrar_pacientes()
-
-pacientes.eliminar_paciente(2)
-
-pacientes.mostrar_pacientes()
---
+++ gestion_pacientes_poo.py
@@ -0,0 +1,108 @@
+import clientes as cl
+import tkinter as tk
+from tkinter import messagebox
+
+pacientes = cl.lista_pacientes()
+
+#funciones de los botones
+def agregar_pacientes():
+    id_paciente = id_entry.get()
+    nombre = nombre_entry.get()
+    raza = raza_entry.get()
+    edad = edad_entry.get()
+    diagnostico = diagnostico_text.get("1.0", tk.END).strip()
+    tratamiento = tratamiento_text.get("1.0", tk.END).strip()
+
+    nuevo_paciente = cl.paciente(id_paciente, nombre, raza, edad, diagnostico, tratamiento)
+    pacientes.agregar_paciente(nuevo_paciente)
+
+    messagebox.showinfo("Información", "Paciente agregado exitosamente")
+
+    # Limpiar los campos
+    id_entry.delete(0, tk.END)
+    nombre_entry.delete(0, tk.END)
+    raza_entry.delete(0, tk.END)
+    edad_entry.delete(0, tk.END)
+    diagnostico_text.delete("1.0", tk.END)
+    tratamiento_text.delete("1.0", tk.END)
+
+def mostrar_pacientes():
+    nueva_ventana = tk.Toplevel(ventana)
+    nueva_ventana.title("Lista de Pacientes")
+    nueva_ventana.geometry("800x600")
+
+    text_lista = tk.Text(nueva_ventana, wrap=tk.WORD, height=20, width=80)
+    text_lista.pack(pady=10)
+   
+    pacientes_texto = pacientes.mostrar_pacientes()
+    text_lista.insert(tk.END, pacientes_texto)
+
+    label = tk.Label(nueva_ventana, text="Lista de Pacientes", font=("Arial",14))
+    label.pack(pady=10)
+
+    tk.Label(nueva_ventana, text="Eliminar Paciente").pack(pady=5)
+    id_eliminar_entry = tk.Entry(nueva_ventana)
+    id_eliminar_entry.pack(pady=5)
+
+    def eliminar_paciente():
+        id_paciente = id_eliminar_entry.get()
+        if id_paciente:
+            pacientes.eliminar_paciente(id_paciente)
+            messagebox.showinfo("Información", f"Paciente con ID {id_paciente} eliminado")
+            id_eliminar_entry.delete(0, tk.END)
+            text_lista.delete("1.0", tk.END)
+            paciente_texto = pacientes.mostrar_pacientes()
+            text_lista.insert(tk.END, paciente_texto)
+        else:
+            messagebox.showwarning("Advertencia", "Por favor, ingrese un ID de paciente.")
+
+    tk.Button(nueva_ventana, text="Eliminar Paciente", command=eliminar_paciente).pack(pady=10)
+
+# ventana principal
+ventana = tk.Tk()
+ventana.title('Sistema gestor de pacientes')
+ventana.geometry('800x600')
+
+icono = tk.PhotoImage(file="icono-vet.png")
+ventana.iconphoto(False, icono)
+
+# boton de cierre de ventana 
+ventana.protocol("WM_DELETE_WINDOW", ventana.destroy)
+
+titulo = tk.Label(ventana, text= 'Sistema gestor de pacientes', fg='red', font=('Arial', 12))
+titulo.pack(pady=10)
+
+img = tk.PhotoImage(file='background_img.png')
+bg_imagen = tk.Label(ventana, image=img)
+bg_imagen.place(x=0,y=50,relwidth=1,relheight=1)
+
+
+# campos de entrada de datos
+tk.Label(ventana, text="Id:").place(x=50, y=50)
+id_entry = tk.Entry(ventana)
+id_entry.place(x=150, y=50, width=150)
+
+tk.Label(ventana, text="Nombre:").place(x=50, y=80)
+nombre_entry = tk.Entry(ventana)
+nombre_entry.place(x=150, y=80, width=150)
+
+tk.Label(ventana, text="Raza:").place(x=50, y=110)
+raza_entry = tk.Entry(ventana)
+raza_entry.place(x=150, y=110, width=150)
+
+tk.Label(ventana, text="Años:").place(x=50, y=140)
+edad_entry = tk.Entry(ventana)
+edad_entry.place(x=150, y=140, width=150)
+
+tk.Label(ventana, text="Diagnostico:").place(x=50, y=170)
+diagnostico_text = tk.Text(ventana, height=4, width=40)
+diagnostico_text.place(x=150, y=170)
+
+tk.Label(ventana, text="Tratamiento:").place(x=50, y=270)
+tratamiento_text = tk.Text(ventana, height=4, width=40)
+tratamiento_text.place(x=150, y=270)
+
+tk.Button(ventana, text="Añadir Paciente", command=agregar_pacientes).place(x=120, y=350)
+tk.Button(ventana, text="Mostrar Lista", command=mostrar_pacientes).place(x=280, y=350)
+
+ventana.mainloop()
